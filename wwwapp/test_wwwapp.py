5e32d4658beb581a3877792c9b137d005b910308
Sebastian Jaszczur <sebastian.jaszczur@gmail.com>
2015-01-03 00:59:13 +0100

    set up unittests

---
+++ wwwapp/test_wwwapp.py
@@ -0,0 +1,10 @@
+from django.test import TestCase, Client
+
+class AnimalTestCase(TestCase):
+    def setUp(self):
+        self.c = Client()
+
+    def test_admin_page(self):
+        """Tests if admin page works"""
+        response = self.client.get('/admin', follow=True)
+        self.assertContains(response, "Django admin", status_code=200)
