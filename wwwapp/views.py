2dfa32b1059216cd5c87723b1b4d09ade7e67501
Marcin Kalinowski <kalmar2718@gmail.com>
2015-04-06 20:30:24 +0200

    HttpResponseRedirect to redirect

--- wwwapp/views.py
+++ wwwapp/views.py
@@ -1,9 +1,8 @@
 #-*- coding: utf-8 -*-
 from django.conf import settings
 from django.contrib import messages
-from django.core.urlresolvers import reverse
-from django.http import HttpResponseRedirect, JsonResponse
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import render, redirect
 
 from wwwapp.models import Article, UserProfile, Workshop
 from wwwapp.forms import ArticleForm, UserProfileForm, UserForm, WorkshopForm
@@ -30,7 +29,7 @@ def get_context(request):
 def profile(request):
     context = get_context(request)
     if not request.user.is_authenticated():
-        return HttpResponseRedirect(reverse('login'))
+        return redirect('login')
     else:
         user_profile = UserProfile.objects.get(user=request.user)
         if request.method == "POST":
@@ -39,15 +38,16 @@ def profile(request):
             if user_form.is_valid() and user_profile_form.is_valid():
                 user_form.save()
                 user_profile_form.save()
+            return redirect('profile')
         else:
             user_form = UserForm(instance=request.user)
             user_profile_form = UserProfileForm(instance=user_profile)
-        context['user_form'] = user_form
-        context['user_profile_form'] = user_profile_form
-        context['title'] = u'Profil'
-        user_form.helper.form_tag = False
-        user_profile_form.helper.form_tag = False
-        return render(request, 'profile.html', context)
+            context['user_form'] = user_form
+            context['user_profile_form'] = user_profile_form
+            context['title'] = u'Profil'
+            user_form.helper.form_tag = False
+            user_profile_form.helper.form_tag = False
+            return render(request, 'profile.html', context)
 
 
 def workshop(request, name=None):
@@ -57,7 +57,7 @@ def workshop(request, name=None):
         workshop = None
         title = u'Nowe warsztaty'
         if not request.user.is_authenticated():
-            return HttpResponseRedirect(reverse('login'))
+            return redirect('login')
         else:
             has_perm_to_edit = True
     else:
@@ -78,7 +78,7 @@ def workshop(request, name=None):
                 user_profile = UserProfile.objects.get(user=request.user)
                 workshop.lecturer.add(user_profile)
                 workshop.save()
-                return HttpResponseRedirect(reverse('workshop', args=(form.instance.name,)))
+                return redirect('workshop', form.instance.name)
         else:
             form = WorkshopForm(instance=workshop)
     else:
@@ -113,7 +113,7 @@ def article(request, name = None):
                 article.modified_by = request.user
                 article.save()
                 form.save_m2m()
-                return HttpResponseRedirect(reverse('article', args=(form.instance.name,)))
+                return redirect('article', form.instance.name)
         else:
             form = ArticleForm(request.user, instance=art)
     else:
@@ -134,7 +134,7 @@ def article_name_list(request):
 
 def your_workshops(request):
     if not request.user.is_authenticated():
-        return HttpResponseRedirect(reverse('login'))
+        return redirect('login')
     context = get_context(request)
     
     workshops = Workshop.objects.filter(lecturer__user=request.user)
