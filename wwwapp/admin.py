665bda5f621e4c45bd50bba821ce7dc58aacbfdd
Sebastian Jaszczur <sebastian.jaszczur@gmail.com>
2015-05-17 23:56:18 +0200

    better management of workshops' statuses in Django Admin

--- wwwapp/admin.py
+++ wwwapp/admin.py
@@ -1,3 +1,4 @@
+#-*- coding: utf-8 -*-
 from django.contrib import admin
 from django.contrib.auth.models import User
 from wwwapp.models import Article, UserProfile, ArticleContentHistory, WorkshopCategory, Workshop, WorkshopType
@@ -11,6 +12,19 @@ class UserProfileInline(admin.StackedInline):
 class UserProfileAdmin(UserAdmin):
     inlines = [ UserProfileInline, ]
 
+class WorkshopAdmin(admin.ModelAdmin):
+    def make_acccepted(modeladmin, request, queryset):
+        queryset.update(status='Z')
+    make_acccepted.short_description = "Zmień status na Zaakceptowane"
+    def make_refused(modeladmin, request, queryset):
+        queryset.update(status='O')
+    make_refused.short_description = "Zmień status na Odrzucone"
+    def make_clear(modeladmin, request, queryset):
+        queryset.update(status=None)
+    make_clear.short_description = "Zmień status na Null"
+    
+    actions = [make_acccepted, make_refused, make_clear]
+
 admin.site.register(User, UserProfileAdmin)
 
 admin.site.register(Article)
@@ -18,4 +32,4 @@ admin.site.register(ArticleContentHistory)
 
 admin.site.register(WorkshopCategory)
 admin.site.register(WorkshopType)
-admin.site.register(Workshop)
+admin.site.register(Workshop, WorkshopAdmin)
