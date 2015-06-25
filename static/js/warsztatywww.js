16f5b7743d0258fa15ec7906f6994e20e875e14d
Michał Zieliński <michal@zielinscy.org.pl>
2015-06-25 21:23:09 +0200

    Reduce loading time on 1Mbps connection from 6s to 2.5s (main page)
    - use Django Compressor for combining JS and CSS
    - move <script> to the end of <body>
    - load CKEditor only if needed

--- static/js/warsztatywww.js
+++ static/js/warsztatywww.js
@@ -1,6 +1,55 @@
 
-$('textarea[data-ckeditor-config]').each(function(){
-    var config = $(this).data('ckeditor-config');
-    config.height = Math.max($(window).height() - 460, 300);
-	CKEDITOR.replace(this, config);
-});
+var editors = $('textarea[data-ckeditor-config]');
+
+function load_script(url, callback) {
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+    script.onload = callback;
+    script.src = url;
+    document.body.appendChild(script);
+}
+
+if(editors.length !== 0) {
+    load_script(ckeditor_js_path, function() {
+        editors.each(function(){
+            var config = $(this).data('ckeditor-config');
+            config.height = Math.max($(window).height() - 460, 300);
+
+	        CKEDITOR.replace(this, config);
+
+        });
+    });
+
+    load_script(ckeditor_highlight_js_path, function() {
+        hljs.initHighlightingOnLoad();
+    });
+}
+
+
+
+function handle_registration_change(workshop_name_txt, register) {
+    var proper_url;
+    if(register) {
+        proper_url = register_to_workshops_url;
+    } else {
+        proper_url = unregister_to_workshops_url;
+    }
+
+    $.ajax({
+        url : proper_url, // the endpoint
+        type : "POST", // http method
+        data : { workshop_name : workshop_name_txt }, // data sent with the post request
+
+        // handle a successful response
+        success : function(json) {
+            if (json.redirect) {
+                window.location.href = json.redirect;
+            } else {
+                $("#" + workshop_name_txt).find(".button-div").replaceWith(json.content);
+            }
+        },
+        error: function(xhr, errmsg, errcode) {
+            $("#"+workshop_name_txt).after('<div class="alert alert-danger fade in"><a href="#" class="close" data-dismiss="alert">&times;</a><strong>Error!</strong> Wystąpił problem przy wysyłaniu danych ('+xhr.status+': '+errcode+').</div>'); // add the error to the dom
+        }
+    });
+}
