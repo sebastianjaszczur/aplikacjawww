661190e4c3197ccbc9ff44a016ed011ae610667a
Marcin Wrochna <mwrochna+git@gmail.com>
2015-06-29 22:23:05 +0200

    long description of motivation letter

--- static/ckeditor/config.js
+++ static/ckeditor/config.js
@@ -5,8 +5,6 @@
 
 CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
-	config.language = 'pl';
+	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
-	config.removePlugins='link';
-	config.extraPlugins='linklocal';
 };
