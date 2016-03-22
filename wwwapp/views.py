70b0d6d91d4d2ec8f298ffc7987ffe66640477b2
Michał Zieliński <michal@zielinscy.org.pl>
2016-03-22 17:53:03 +0100

    remove conflicting workaround for #65

--- wwwapp/views.py
+++ wwwapp/views.py
@@ -578,5 +578,5 @@ def as_article(name):
         return article(request, name)
     return page
 
-index = lazy(lambda: as_article("index"))
-template_for_workshop_page = lazy(lambda: as_article("template_for_workshop_page"))
+index = as_article("index")
+template_for_workshop_page = as_article("template_for_workshop_page")
