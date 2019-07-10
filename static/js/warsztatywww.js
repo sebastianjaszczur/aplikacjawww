fdea3cedd968e4e2982b8a2bd5fdefd656b91b33
Grzegorz Uriasz <gorbak25@gmail.com>
2019-07-10 21:39:34 +0200

    Add data tables

--- static/js/warsztatywww.js
+++ static/js/warsztatywww.js
@@ -107,6 +107,14 @@ function handle_registration_change(workshop_name_txt, register) {
     });
 }
 
+$(document).ready( function () {
+    $('.data-table').each(
+      function() {
+          $( this ).DataTable();
+      }
+    );
+} );
+
 // Automatically hide 'Saved successfully' alerts after 4 seconds
 $(".alert").delay(4000).fadeTo(500, 0).slideUp(500, function(){
     $(this).remove();
