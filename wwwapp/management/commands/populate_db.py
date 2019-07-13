461b4394fd1c4389c23ca00ecbbef89741d29972
Grzegorz Uriasz <gorbak25@gmail.com>
2019-07-13 03:13:27 +0200

    Polishing

--- wwwapp/management/commands/populate_db.py
+++ wwwapp/management/commands/populate_db.py
@@ -1,18 +1,26 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
-from wwwapp.models import UserProfile, UserInfo, Article, ArticleContentHistory, Workshop, WorkshopCategory, WorkshopType, WorkshopParticipant
+from wwwapp.models import UserProfile, UserInfo, Article, ArticleContentHistory, Workshop, WorkshopCategory, \
+    WorkshopType, WorkshopParticipant
 from faker import Faker
-from faker.providers import profile, person, date_time
+from faker.providers import profile, person, date_time, internet
 import random
 
 """
 Command implementing database population. 
 Useful for making test fixtures. 
 """
+
+
 class Command(BaseCommand):
     args = ''
     help = 'Populate the database with data for development'
     LOCALE = 'pl_PL'
+    NUM_OF_USERS = 50
+    NUM_OF_WORKSHOPS = 5
+    NUM_OF_ARTICLES = 2
+    NUM_OF_CATEGORIES = 2
+    NUM_OF_TYPES = 2
 
     """
     Constructor of the command
@@ -22,6 +30,7 @@ class Command(BaseCommand):
         fake.add_provider(profile)
         fake.add_provider(person)
         fake.add_provider(date_time)
+        fake.add_provider(internet)
         self.fake = fake
 
         super().__init__()
@@ -29,33 +38,35 @@ class Command(BaseCommand):
     """
     Create and returns a fake random user.
     """
-    def fake_user(self) -> UserProfile:
+    def fake_user(self) -> (User, UserProfile):
         profile_data = self.fake.profile()
-        u = User.objects.create_user(profile_data['username'], profile_data['mail'], 'password')
-        u.save()
+        user = User.objects.create_user(profile_data['username'], profile_data['mail'], 'password')
+        user.first_name = self.fake.first_name()
+        user.last_name = self.fake.last_name()
+        user.save()
 
         info = UserInfo(pesel=profile_data['ssn'],
                         address=profile_data['address'],
                         comments=self.fake.text(100))
         info.save()
 
-        profile = UserProfile(user=u,
-                              user_info=info,
-                              gender=profile['sex'],
-                              school="TEST",
-                              matura_exam_year=self.fake.date_this_year().year,
-                              how_do_you_know_about=self.fake.text(),
-                              profile_page=self.fake.text(),
-                              cover_letter=self.fake.text())
+        user_profile = UserProfile(user=user,
+                                   user_info=info,
+                                   gender=profile_data['sex'],
+                                   school="TEST",
+                                   matura_exam_year=self.fake.date_this_year().year,
+                                   how_do_you_know_about=self.fake.text(),
+                                   profile_page=self.fake.text(),
+                                   cover_letter=self.fake.text())
 
-        profile.save()
-        return u
+        user_profile.save()
+        return user, user_profile
 
     """
     Creates and returns a fake random article with a random edit history
     """
     def fake_article(self, users) -> Article:
-        article = Article(name=self.fake.text(),
+        article = Article(name=self.fake.uri_page(),
                           title=self.fake.text(),
                           content=self.fake.paragraph(),
                           modified_by=random.choice(users),
@@ -76,7 +87,7 @@ class Command(BaseCommand):
     Creates and returns a random category
     """
     def fake_category(self) -> WorkshopCategory:
-        c = WorkshopCategory(year=2019, name=self.fake.text())
+        c = WorkshopCategory(year=2019, name=self.fake.text(10))
         c.save()
         return c
 
@@ -84,26 +95,22 @@ class Command(BaseCommand):
     Creates and returns a random type
     """
     def fake_type(self) -> WorkshopType:
-        c = WorkshopType(year=2019, name=self.fake.text())
+        c = WorkshopType(year=2019, name=self.fake.text(10))
         c.save()
         return c
 
     """
     Creates a fake random workshops with 5 random participants
     """
-    def fake_workshop(self, users, types, categories) -> Workshop:
-        participants = random.choices(users, k=5)
-        workshop = Workshop(name=self.fake.text(),
-                            title=self.fake.title(),
+    def fake_workshop(self, lecturer, participants, types, categories) -> Workshop:
+        workshop = Workshop(name=self.fake.uri_page(),
+                            title=self.fake.text(10),
                             proposition_description=self.fake.paragraph(),
                             type=random.choice(types),
-                            category=random.choice(categories),
-                            lecturer=random.choice(users),
                             status='Z',
                             page_content=self.fake.paragraph(),
                             page_content_is_public=True,
-                            is_qualifying=True,
-                            participants=participants
+                            is_qualifying=True
                             )
         workshop.save()
 
@@ -112,6 +119,10 @@ class Command(BaseCommand):
                                        participant=participant,
                                        comment=self.fake.paragraph())
             info.save()
+
+        workshop.category.add(random.choice(categories))
+        workshop.lecturer.add(lecturer)
+        workshop.save()
 
         return workshop
 
@@ -122,21 +133,29 @@ class Command(BaseCommand):
         User.objects.create_superuser("admin", "admin@admin.admin", "admin")
 
         users = []
-        for i in range(50):
-            users.append(self.fake_user())
+        user_profiles = []
+        for i in range(self.NUM_OF_USERS):
+            (user, user_profile) = self.fake_user()
+            users.append(user)
+            user_profiles.append(user_profile)
 
         articles = []
-        for i in range(2):
+        for i in range(self.NUM_OF_ARTICLES):
             articles.append(self.fake_article(users))
 
         types = []
-        for i in range(2):
+        for i in range(self.NUM_OF_TYPES):
             types.append(self.fake_type())
 
         categories = []
-        for i in range(2):
+        for i in range(self.NUM_OF_CATEGORIES):
             categories.append(self.fake_category())
 
+        lecturers = user_profiles[:self.NUM_OF_WORKSHOPS]
+        participants = user_profiles[self.NUM_OF_WORKSHOPS:]
+        participants = [participants[self.NUM_OF_WORKSHOPS * i:self.NUM_OF_WORKSHOPS * (i + 1)] for i in
+                        range(len(participants) // self.NUM_OF_WORKSHOPS)]
+
         workshops = []
-        for i in range(5):
-            workshops.append(self.fake_workshop(users, types, categories))
+        for lecturer, participants in zip(lecturers, participants):
+            workshops.append(self.fake_workshop(lecturer, participants, types, categories))
